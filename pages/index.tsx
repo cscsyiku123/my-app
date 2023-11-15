9a65ff6431821e2a9b1488888b97ad25f2518261
张权 <zhangquan@xiaohongshu.com>
2023-11-15 20:14:36 +0800

    1

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,10 +10,10 @@ export default function () {
                 <div>直播</div>
                 <div>游戏中心</div>
                 <div>会员购</div>
-                <div className="searchTopBar">
-                    <input type="text" placeholder="T1训练营" onFocus={()=>setSearchDetailShow(!searchDetailShow)}/>
+                <div className="searchTopBar h-[40px] w-[300px] bg-gray-100 rounded-2xl flex items-center justify-around relative">
+                    <input className=" bg-opacity-100 w-[250px]" type="text" placeholder="T1训练营" onFocus={()=>setSearchDetailShow(!searchDetailShow)}/>
                     <div className="searchIcon"></div>
-                    {searchDetailShow && <div></div>}
+                    {searchDetailShow && <div className="w-[300px] absolute top-[100px]">12123123123133</div>}
                 </div>
             </div>
         </>
